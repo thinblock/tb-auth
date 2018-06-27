3c4b04ca321f1ab6419c4e4199abd0df973bb2ae
Hamza Baig <hamzabaig18@gmail.com>
2018-06-27 21:15:20 +0500

    adds user and app model

---
+++ src/models/app.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const AppSchema = new Schema({
+  name: {type: String, required: true},
+  client_id: {type: String, unique: true, required: true},
+  client_secret: {type: String, required: true},
+  scope: {type: String, required: true},
+  user: {type: mongoose.Schema.Types.ObjectId, ref: 'users', required: true},
+  created_at: {type: Date, default: Date.now},
+  updated_at: {type: Date, default: Date.now}
+}, {collection: 'apps'});
+
+module.exports = mongoose.model('apps', UserSchema);
--- src/models/user.js
+++ src/models/user.js
@@ -1,9 +1,18 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-// set up a mongoose model and pass it using module.exports
-module.exports = mongoose.model(
-  'User',
-  new Schema({ username: String, password: String, token: String }),
-  'User'
-);
+const UserSchema = new Schema({
+  name: String,
+  email: {type: String, unique: true},
+  email_verified: {type: Boolean, default: false},
+  auth_provider: {
+    type: String,
+    required: true,
+    enum: ['EMAIL', 'GOOGLE', 'FACEBOOK']
+  },
+  password: String,
+  created_at: {type: Date, default: Date.now},
+  updated_at: {type: Date, default: Date.now}
+}, {collection: 'users'});
+
+module.exports = mongoose.model('users', UserSchema);
